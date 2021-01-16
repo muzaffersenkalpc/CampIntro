6a432c5228c6dc946d12ac8f33734cbebfa13381
MuzafferSenkal <muzaffersenkalpcmail.com>
2021-01-16 23:46:20 +0300

    16 ocak, git with visual studio.

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -17,7 +17,7 @@ namespace DegerVeReferansTipler
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
-            //sayilar1[0] = 999
+            //sayilar1[0] = 9999
         }
     }
 }
