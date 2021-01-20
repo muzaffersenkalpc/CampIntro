46bd1e8c5054be309b94f02109cb676ca1b0de56
MuzafferSenkal <muzaffersenkalpcmail.com>
2021-01-20 23:56:03 +0300

    20 ocak.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation -->  nesne gönderimine denir. not included int,string,string,int, just product class gibi.
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, Ue = 35, ProductName = "Kalem" nitsInStock = 5, UnitPric };
+
+
+            //PascalCase     //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product2);
+
+            //int,double,bool.... değer tip
+            //diziler,class abstract class, interface.... referans tip.
+        }
+    }
+}
