cb26056cdbbf2bb14ce4c23348cdf33284afee60
MuzafferSenkal <muzaffersenkalpcmail.com>
2021-01-25 22:51:11 +0300

    MyDictionary Added!

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+        //constructor
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;  //önce ki diziyi hafızaya kaydettik.
+            items = new T[items.Length + 1];  //1 değer fazlalı dizi oluşturduk.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];  //hafızada ki verileri, yeni diziye aktardık.
+            }
+
+            items[items.Length - 1] = item;  //0 dan başladığı için, son değere gelen item değerimizi ekledik.
+        }
+
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Engin");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Muzo");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
